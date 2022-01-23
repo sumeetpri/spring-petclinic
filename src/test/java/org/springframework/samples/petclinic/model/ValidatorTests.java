043ffe2d2ec79dbd0ba4687334adc59843f06967
Sumeet Kumar <mailtosumeet88@gmail.com>
2022-01-22 19:45:08 -0800

    Update ValidatorTests.java

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -44,7 +44,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("Break");
+		person.setFirstName("");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
